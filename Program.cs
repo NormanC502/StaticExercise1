26004d4114efbaaf9d47230062bdd795c3e453da
Norman Calhoun <shakauluz73@yahoo.com>
2021-02-04 19:22:19 -0500

    complete

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise1
+{
+    class Program:TempConverter
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("These are the conversions!");
+
+            var celsius = TempConverter.FahrenheitToCelsius(75);
+
+            Console.WriteLine($"What is your Fahrenheit temperature?\n This coverts to {celsius} degrees celsius!");
+
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(25);
+
+            Console.WriteLine($"What is your Celsius temperature?\n This coverts to {fahrenheit} degrees fahreheit!");
+        }
+    }
+}
